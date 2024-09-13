ffeb7c7ddf3d33448e03fda810b4fcab5f4029c5
Scott Crowder <scrowder@gmail.com>
2024-09-13 09:30:06 -0500

    Initial Commit

---
+++ LOTR.Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,20 @@
+//
+// File: LOTR_ConverterApp.swift
+// Project: LOTR.Converter
+// 
+// Created by SCOTT CROWDER on 9/13/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
